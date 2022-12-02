5bbae1cd4c42caef1ca2782c364cf433cccd0ce4
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-12-02 08:42:31 +0100

    config

--- Config/index.js
+++ Config/index.js
@@ -1,11 +1,19 @@
 const Cloud = require('@google-cloud/storage')
 const path = require('path')
-const serviceKey = path.join("d:\\Bitbucket\\all.configuration\\ptplacesprod-storage-serviceaccount.json")
+//const serviceKey = path.join("d:\\Bitbucket\\all.configuration\\ptplacesprod-storage-serviceaccount.json")
+const serviceKey = path.join("d:\\Bitbucket\\all.configuration\\ptplacesdev-serviceaccount.json")
 
+let bucketName = 'none'
 const { Storage } = Cloud
+if (process.env.NODE_ENV == 'development') {
+  bucketName = 'ptplacesprod'
+}
+else {
+  bucketName = placesdevvisits
+}
 const storage = new Storage({
   keyFilename: serviceKey,
-  projectId: 'ptplacesprod',
+  projectId: bucketName,
 })
 
 module.exports = storage
