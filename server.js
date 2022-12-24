79ad0d27cc5be67b4bb6d89874ec017090db44f3
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-12-24 14:10:07 +0100

    filesize

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const db = getFirestore();
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+    fileSize: 10 * 1024 * 1024, // no larger than 5mb, you can change as needed.
   },
 });
 
